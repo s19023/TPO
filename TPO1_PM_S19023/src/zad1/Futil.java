6a84e6c8cdd7e81831fa2f1c30986669e772de39
michalpazur <michal.pazur1@gmail.com>
2020-03-07 23:13:55 +0100

    Changed Futil: added walkFileTree

--- TPO1_PM_S19023/src/zad1/Futil.java
+++ TPO1_PM_S19023/src/zad1/Futil.java
@@ -1,9 +1,34 @@
 package zad1;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class Futil
 {
     public static void processDir(String dirName, String resultFileName)
     {
+        try
+        {
+            File resultFile = new File(resultFileName);
+            resultFile.createNewFile();
+            Path directoryPath = Paths.get(dirName);
 
+            Files.walkFileTree(directoryPath, new SimpleFileVisitor<Path>()
+            {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException
+                {
+                    
+                    return super.visitFile(path, basicFileAttributes);
+                }
+            });
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
