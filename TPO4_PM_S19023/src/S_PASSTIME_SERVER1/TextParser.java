42aace8b4b988938982146485e644d1f7d7e60d2
s19023 <s19023@pja.edu.pl>
2020-04-21 20:07:36 +0200

    Add: files from TPO3

---
+++ TPO4_PM_S19023/src/S_PASSTIME_SERVER1/TextParser.java
@@ -0,0 +1,101 @@
+package PASSTIME1;
+
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Locale;
+
+public class TextParser
+{
+    static String getFormattedString(long value, ChronoUnit unit)
+    {
+        value = value > 19 || value < 9 ? value % 10 : value;
+        switch(unit)
+        {
+            case MINUTES:
+                return getMinutes(value);
+            case HOURS:
+                return getHours(value);
+            case DAYS:
+                return getDays(value);
+            case MONTHS:
+                return getMonths(value);
+            case YEARS:
+                return getYears(value);
+            default:
+                return "???";
+        }
+    }
+
+    static String getDaysBetween(long totalDaysBetween)
+    {
+        return "\n - mija: " + totalDaysBetween + " " + TextParser.getFormattedString(totalDaysBetween, ChronoUnit.DAYS) + String.format(Locale.US,", tygodni %.2f", totalDaysBetween/7.0);
+    }
+
+    static String getHoursBetween(long hoursBetween, long minutesBetween)
+    {
+        return "\n - godzin: " + hoursBetween + ", minut: " + minutesBetween;
+    }
+
+    static String getCalendarDaysBetween(Period p)
+    {
+        String toReturn = "\n - kalendarzowo: ";
+        for (TemporalUnit t : p.getUnits())
+        {
+            int value = (int) p.get(t);
+
+            if (value > 0)
+                toReturn += value + " " + getFormattedString(value, (ChronoUnit) t) + ", ";
+        }
+
+        return toReturn.substring(0, toReturn.length() - 2);
+    }
+
+    private static String getMinutes(long value)
+    {
+        if (value == 0 || value > 4)
+            return "minut";
+        else if (value == 1)
+            return "minuta";
+        else
+            return "minuty";
+    }
+
+    private static String getHours(long value)
+    {
+        if (value == 0 || value > 4)
+            return "godzin";
+        else if (value == 1)
+            return "godzina";
+        else
+            return "godziny";
+    }
+
+    private static String getDays(long value)
+    {
+        if (value == 0 || value > 1)
+            return "dni";
+        else
+            return "dzień";
+    }
+
+    private static String getMonths(long value)
+    {
+        if (value == 0 || value > 4)
+            return "miesięcy";
+        else if (value == 1)
+            return "miesiąc";
+        else
+            return "miesiące";
+    }
+
+    private static String getYears(long value)
+    {
+        if (value == 0 || value > 4)
+            return "lat";
+        else if (value == 1)
+            return "rok";
+        else
+            return "lata";
+    }
+}
--- TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Time.java
+++ TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Time.java
@@ -7,5 +7,56 @@
 package S_PASSTIME_SERVER1;
 
 
-public class Time {
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+
+public class Time
+{
+    private static Locale pl = new Locale("pl");
+    private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy (EEEE)", pl);
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy (EEEE) 'godz.' HH:mm", pl);
+    private static ZoneId timeZone = ZoneId.of("Europe/Warsaw");
+
+    static String passed(String from, String to)
+    {
+        String toReturn = "";
+        try
+        {
+            LocalDate dateFrom = LocalDate.parse(from);
+            LocalDate dateTo = LocalDate.parse(to);
+            toReturn = "Od " + dateFrom.format(dateFormatter);
+            toReturn += " do " + dateTo.format(dateFormatter);
+
+            toReturn += TextParser.getDaysBetween(ChronoUnit.DAYS.between(dateFrom, dateTo));
+            toReturn += TextParser.getCalendarDaysBetween(Period.between(dateFrom, dateTo));
+        }
+        catch(DateTimeParseException e)
+        {
+            try
+            {
+                LocalDateTime dateFrom = LocalDateTime.parse(from);
+                LocalDateTime dateTo = LocalDateTime.parse(to);
+                ZonedDateTime zonedDateFrom = ZonedDateTime.of(dateFrom, timeZone);
+                ZonedDateTime zonedDateTo = ZonedDateTime.of(dateTo, timeZone);
+                LocalDate localDateFrom = dateFrom.toLocalDate();
+                LocalDate localDateTo = dateTo.toLocalDate();
+
+                toReturn = "Od " + zonedDateFrom.format(dateTimeFormatter);
+                toReturn += " do " + zonedDateTo.format(dateTimeFormatter);
+
+                toReturn += TextParser.getDaysBetween(ChronoUnit.DAYS.between(localDateFrom, localDateTo));
+                toReturn += TextParser.getHoursBetween(ChronoUnit.HOURS.between(zonedDateFrom, zonedDateTo), ChronoUnit.MINUTES.between(zonedDateFrom, zonedDateTo));
+                toReturn += TextParser.getCalendarDaysBetween(Period.between(localDateFrom, localDateTo));
+            }
+            catch(DateTimeParseException ex)
+            {
+                return "*** " + ex.toString();
+            }
+        }
+
+        return toReturn;
+    }
 }
