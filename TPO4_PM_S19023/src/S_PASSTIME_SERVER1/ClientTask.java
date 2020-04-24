407b3e5bde64742e2dbc8c347303fbb9051cca21
s19023 <s19023@pja.edu.pl>
2020-04-24 12:23:29 +0200

    Add: Misc IntellIJ files

--- TPO4_PM_S19023/src/S_PASSTIME_SERVER1/ClientTask.java
+++ TPO4_PM_S19023/src/S_PASSTIME_SERVER1/ClientTask.java
@@ -7,5 +7,33 @@
 package S_PASSTIME_SERVER1;
 
 
-public class ClientTask {
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class ClientTask extends FutureTask<String>
+{
+    private Client c;
+    private List<String> reqs;
+    private boolean showSendRes;
+
+    public static ClientTask create(Client c, List<String> reqs, boolean showSendRes)
+    {
+        return new ClientTask(c, reqs, showSendRes);
+    }
+
+    public ClientTask(Client c, List<String> reqs, boolean showSendRes)
+    {
+        this.c = c;
+        this.reqs = reqs;
+        this.showSendRes = showSendRes;
+
+        c.connect();
+        c.send("login " + id);
+    }
+
+    public String get() throws InterruptedException, ExecutionException
+    {
+
+    }
 }
