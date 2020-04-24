59cd45a54373fd212bc246edfe88afa5d814ac34
s19023 <s19023@pja.edu.pl>
2020-04-24 18:23:18 +0200

    Add: base for reading from and writing to clients in Server

--- TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Server.java
+++ TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Server.java
@@ -7,10 +7,13 @@
 package S_PASSTIME_SERVER1;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.*;
 
 public class Server
 {
@@ -18,6 +21,7 @@ public class Server
     private int port;
     private List<String> serverLogs = new ArrayList<>();
     private Map<String, List<String>> clientLogs = new HashMap<>();
+    private boolean isRunning = false;
 
     public Server(String host, int port)
     {
@@ -27,12 +31,61 @@ public class Server
 
     void startServer()
     {
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open())
+        {
+            serverChannel.bind(new InetSocketAddress(host, port));
+            serverChannel.configureBlocking(false);
+            Selector selector = Selector.open();
+            SelectionKey selectionKey = serverChannel.register(selector, SelectionKey.OP_ACCEPT);
 
+            new Thread(() ->
+            {
+                while(isRunning)
+                {
+                    try
+                    {
+                        selector.select();
+                        Set keys = selector.selectedKeys();
+                        Iterator keyIterator = keys.iterator();
+                        while(keyIterator.hasNext())
+                        {
+                            SelectionKey key = (SelectionKey) keyIterator.next();
+                            keyIterator.remove();
+                            if (key.isAcceptable())
+                            {
+                                SocketChannel clientChannel = serverChannel.accept();
+                                clientChannel.configureBlocking(false);
+                                clientChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                            }
+                            else if (key.isReadable())
+                            {
+                                SocketChannel clientChannel = (SocketChannel) key.channel();
+                                //TODO: reading client requests
+                            }
+                            else if (key.isWritable())
+                            {
+                                SocketChannel clientChannel = (SocketChannel) key.channel();
+                                //TODO: writing data to clients
+                            }
+                        }
+                    }
+                    catch(IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     void stopServer()
     {
-
+        isRunning = false;
     }
 
     private String getClientLogs(String client)
