9ca9879648eb67960993462aadb4a36d870e9aa2
s19023 <s19023@pja.edu.pl>
2020-04-24 12:18:00 +0200

    Basic implementation of Server class

--- TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Server.java
+++ TPO4_PM_S19023/src/S_PASSTIME_SERVER1/Server.java
@@ -7,5 +7,40 @@
 package S_PASSTIME_SERVER1;
 
 
-public class Server {
+import java.util.List;
+import java.util.Map;
+
+public class Server
+{
+    private String host;
+    private int port;
+    private Map<String, List<String>> userLogs;
+    private List<String> serverLogs;
+
+    public Server(String host, int port)
+    {
+        this.host = host;
+        this.port = port;
+    }
+
+    void startServer()
+    {
+
+    }
+
+    void stopServer()
+    {
+
+    }
+
+    String getServerLog()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String logEntry : serverLogs)
+        {
+            stringBuilder.append(logEntry);
+            stringBuilder.append('\n');
+        }
+        return stringBuilder.toString();
+    }
 }
