ddc02a41d59b575e8bb90c8acc282d3cfc710030
s19023 <s19023@pja.edu.pl>
2020-05-14 23:42:37 +0200

    Add: ParametersServlet

---
+++ TPO5_PM_S19023/src/zad1/ParametersServlet.java
@@ -0,0 +1,57 @@
+package zad1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ParametersServlet extends HttpServlet
+{
+    private ServletContext context;
+
+    @Override
+    public void init()
+    {
+        context = getServletContext();
+    }
+
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String charset = request.getHeader("charset");
+        response.setCharacterEncoding(charset);
+        HttpSession session = request.getSession();
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<center><h2>Książki?</h2></center>");
+        writer.println("<form method = \"post\">");
+        writer.print("<input type = \"text\" size = 30 name = \"search\"");
+        String val = (String) session.getAttribute("search");
+        if (val != null)
+            writer.print(" value = \"" + val + "\"");
+        writer.println("><br>");
+        writer.println("<input type = \"submit\" value = \"Wyszukaj\">");
+        writer.println("</form");
+
+        String search = request.getParameter("search");
+        if (search == null)
+            return;
+        session.setAttribute("search", search);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        handleRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        handleRequest(req, resp);
+    }
+}
